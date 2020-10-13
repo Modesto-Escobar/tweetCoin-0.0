e476b1fbb504246c97ffbb3b1c590bbbcd9151d9
José Luis Alonso Berrocal <berrocal@usal.es>
2020-10-13 12:20:22 +0200

    Modificado load_tweets y user_tweet

--- R/coin_twitter.R
+++ R/coin_twitter.R
@@ -10,43 +10,100 @@ credentials <- function(file = "credentials.txt") {
     assign("token", TT, envir = .GlobalEnv)
   }
 
-load_tweets <- function(name, type = "user"){
+load_tweets <- function(name, type = "user", format = "binary"){
+  
+  if (format == 'binary')
+  {
+    extension <- ".dat"
+  } else  if (format == "delimited")
+  {
+    extension <- ".csv"
+  }
+  
   if (type == "user")
   {
-    output_file_name <- paste0("user_tweets_", name, ".csv")
+    output_file_name <- paste0("user_tweets_", name, extension)
   }
   else if (type == "search")
   {
-    output_file_name <- paste0("search_tweets_", name, ".csv")
+    output_file_name <- paste0("search_tweets_", name, extension)
   }
   else if (type == "stream")
   {
-    output_file_name <- paste0("stream_tweets_", name, ".csv")
+    output_file_name <- paste0("stream_tweets_", name, extension)
   }
-  data_tweets<-read_twitter_csv(file = output_file_name, unflatten = FALSE)
-  return(data_tweets)
+  
+  if (format == "binary")
+  {
+    load(output_file_name, .GlobalEnv )
+    
+  } else if (format == "delimites")
+  {
+    data_tweets<-read_twitter_csv(file = output_file_name, unflatten = FALSE)
+    return(data_tweets)
+    
+  }
+  
+  
 }
 
-user_tweet <- function(user, maxtweets = 100, home = FALSE, parse = TRUE, check = TRUE, token = NULL, include_rts = FALSE, output_file_name = NULL){
+user_tweet <- function(user, maxtweets = 100, home = FALSE, parse = TRUE, check = TRUE, token = NULL, include_rts = FALSE, output_file_name = NULL, format = "binary"){
+  if (format == 'binary')
+  {
+    extension <- ".dat"
+  } else  if (format == "delimited")
+  {
+    extension <- ".csv"
+  }
+  
   if(is.null(output_file_name)){
-    output_file_name <- paste0("user_tweets_", user, ".csv")
+    output_file_name <- paste0("user_tweets_", user, extension)
   }
   
   if(!(output_file_name %in% list.files())){
     datos_new <- get_timeline(user = user, n = maxtweets, home = home, parse = parse, check = check, token = token, include_rts = include_rts)
-    write_as_csv(x = datos_new, file_name = output_file_name)
+    if (format == 'binary')
+    {
+      save( datos_new, file=output_file_name)
+    } else  if (format == "delimited")
+    {
+      write_as_csv(x = datos_new, file_name = output_file_name)
+    }
+    
+    
     print(paste("Numero de tweets nuevos:", nrow(datos_new)))
     print("Nuevo fichero creado")
   }else{
-    datos_old<-read_twitter_csv(file = output_file_name, unflatten = TRUE)
+    if (format == 'binary')
+    {
+      load(output_file_name, .GlobalEnv )
+      datos_old <- datos_new
+      
+      
+    } else  if (format == "delimited")
+    {
+      datos_old<-read_twitter_csv(file = output_file_name, unflatten = TRUE)
+    }
+    
+    
     ultimo_id <- datos_old[nrow(datos_old), ]$status_id
     ultimo_id = toString(as.bigz(ultimo_id) + 1)
-    datos_new <- get_timeline(user = user, n = maxtweets, max_id = ultimo_id,
+    datos_new <- get_timeline(user = user, n = maxtweets, since_id = ultimo_id,
                               home = home, parse = parse, check = check, token = token, include_rts = include_rts)
     write_as_csv(x = datos_new, file_name = "./tmp.csv")
     datos_new<-read_twitter_csv(file = "./tmp.csv", unflatten = TRUE)
-    datos_concatenados <- rbind(datos_old, datos_new)
-    write_as_csv(x = datos_concatenados, file_name = output_file_name)
+    datos_new <- rbind(datos_old, datos_new)
+    if (format == "binary")
+    {
+      datos_new <- 
+      save( datos_new, file=output_file_name)
+      
+    } else if (format == "delimites")
+    {
+      write_as_csv(x = datos_new, file_name = output_file_name)
+      
+    }
+    
     print(paste("Numero total de tweets:", nrow(datos_concatenados)))
     print(paste("Numero de tweets nuevos:", nrow(datos_new)))
   }
