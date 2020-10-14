5eb1c8907920d06e34bd84e3271a7166eb6fed78
José Luis Alonso Berrocal <berrocal@usal.es>
2020-10-14 12:38:36 +0200

    Añadido en search_tweet y stream_tweet la opcion de formato de fichero. Uniformizado el nombre de la variable con los tweets a tuits, para que se identifique mejor.

--- R/coin_twitter.R
+++ R/coin_twitter.R
@@ -33,16 +33,16 @@ user_tweet <- function(user, maxtweets = 100, home = FALSE, parse = TRUE, check 
   if(is.null(output_file_name)) output_file_name <- paste0("user_tweets_", user, extension)
   
   if(!(output_file_name %in% list.files())){
-    datos_new <- get_timeline(user = user, n = maxtweets, home = home, parse = parse, check = check, token = token, include_rts = include_rts)
-    if (format[1] == 'binary') save( datos_new, file=output_file_name)
-    else  if (format[1] == "delimited") write_as_csv(x = datos_new, file_name = output_file_name)
-    print(paste("Numero de tweets nuevos:", nrow(datos_new)))
+    tuits <- get_timeline(user = user, n = maxtweets, home = home, parse = parse, check = check, token = token, include_rts = include_rts)
+    if (format[1] == 'binary') save( tuits, file=output_file_name)
+    else  if (format[1] == "delimited") write_as_csv(x = tuits, file_name = output_file_name)
+    print(paste("Numero de tweets nuevos:", nrow(tuits)))
     print("Nuevo fichero creado")
   }
   else {
     if (format[1] == 'binary'){
       load(output_file_name, .GlobalEnv )
-      datos_old <- datos_new
+      datos_old <- tuits
     } 
     else  if (format[1] == "delimited") datos_old<-read_twitter_csv(file = output_file_name, unflatten = TRUE)
 
@@ -61,37 +61,63 @@ user_tweet <- function(user, maxtweets = 100, home = FALSE, parse = TRUE, check 
   }
 }
 
-search_tweet <- function(search, maxtweets = 300, type = "recent", include_rts = TRUE, geocode = NULL, max_id = NULL, 
-                         parse = TRUE, token = NULL, retryonratelimit = FALSE, verbose = TRUE, output_file_name = NULL) {
-  if(is.null(output_file_name)) output_file_name <- paste0("search_tweets_", search, ".csv")
+search_tweet <- function(search, maxtweets = 300, type = "recent", include_rts = TRUE, geocode = NULL, since_id = NULL, 
+                         parse = TRUE, token = NULL, retryonratelimit = FALSE, verbose = TRUE, output_file_name = NULL, format = c("binary", "delimited")) {
+  
+                           
+   if (format[1] == 'binary') extension <- ".dat"
+   else  if (format[1] == "delimited") extension <- ".csv"
+   
+   if(is.null(output_file_name)) output_file_name <- paste0("search_tweets_", search, extension)
+                           
   if(!(output_file_name %in% list.files())) {
-    datos_new <- search_tweets(search, n = maxtweets, type = type, include_rts = include_rts, geocode = geocode, max_id = max_id, parse = parse, token = token, retryonratelimit = retryonratelimit, verbose = verbose)
-    write_as_csv(x = datos_new, file_name = output_file_name)
+    tuits <- search_tweets(search, n = maxtweets, type = type, include_rts = include_rts, geocode = geocode, since_id = since_id, parse = parse, token = token, retryonratelimit = retryonratelimit, verbose = verbose)
     
-    print(paste("Numero de tweets nuevos:", nrow(datos_new)))
+    if (format[1] == 'binary') save( tuits, file=output_file_name)
+    else  if (format[1] == "delimited") write_as_csv(x = tuits, file_name = output_file_name)
+    
+    print(paste("Numero de tweets nuevos:", nrow(tuits)))
     print("Nuevo fichero creado")
   }
   else {
-    datos_old<-read_twitter_csv(file = output_file_name, unflatten = TRUE)
+    
+    if (format[1] == 'binary'){
+      load(output_file_name, .GlobalEnv )
+      datos_old <- tuits
+    } 
+    else  if (format[1] == "delimited") datos_old<-read_twitter_csv(file = output_file_name, unflatten = TRUE)
+    
+    
     ultimo_id <- datos_old[1,]$status_id
     ultimo_id = toString(as.bigz(ultimo_id) + 1)
     datos_new <- search_tweets(search, n = maxtweets, type = type, include_rts = include_rts, geocode = geocode, since_id = ultimo_id, parse = parse, token = token, retryonratelimit = retryonratelimit, verbose = verbose)
     write_as_csv(x = datos_new, file_name = "./tmp.csv")
     datos_new<-read_twitter_csv(file = "./tmp.csv", unflatten = TRUE)
-    datos_concatenados <- rbind(datos_old, datos_new)
-    write_as_csv(x = datos_concatenados, file_name = output_file_name)
-    print(paste("Numero total de tweets:", nrow(datos_concatenados)))
+    tuits <- rbind(datos_new, datos_old)
+    
+    if (format[1] == "binary") save( tuits, file=output_file_name)
+    else if (format[1] == "delimited") write_as_csv(x = tuits, file_name = output_file_name)
+    
+    print(paste("Numero total de tweets:", nrow(tuits)))
     print(paste("Numero de tweets nuevos:", nrow(datos_new)))
   }
 }
 
-stream_tweet <- function(search, timeout = 120, parse = TRUE, token = NULL, output_file_name = search, verbose = TRUE, dir = NULL) {
+stream_tweet <- function(search, timeout = 120, parse = TRUE, token = NULL, output_file_name = search, verbose = TRUE, dir = NULL, format = c("binary", "delimited")) {
     datos <- stream_tweets(search, timeout = timeout, parse = parse, token = token, file_name = output_file_name, verbose = verbose)
     output_file_name_json <- paste0(output_file_name, ".json")
-    datos_new <- parse_stream(output_file_name_json)
-    output_file_name_csv <- paste0("stream_tweets_", output_file_name, ".csv")
-    write_as_csv(x = datos_new, file_name = output_file_name_csv)
-    print(paste("Numero de tweets:", nrow(datos_new)))
+    tuits <- parse_stream(output_file_name_json)
+    
+    if (format[1] == 'binary') extension <- ".dat"
+    else  if (format[1] == "delimited") extension <- ".csv"
+    
+    output_file_name <- paste0("stream_tweets_", output_file_name, extension)
+    
+    if (format[1] == "binary") save( tuits, file=output_file_name)
+    else if (format[1] == "delimited") write_as_csv(x = tuits, file_name = output_file_name)
+    
+    
+    print(paste("Numero de tweets:", nrow(tuits)))
     
     if (file.exists(output_file_name_json)) file.remove(output_file_name_json)
 }
